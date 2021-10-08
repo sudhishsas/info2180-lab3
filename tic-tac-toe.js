0cbb101d0c610157c5dcf75059d54121738166a1
Sudhish Sepaul <sudhish.sepaul@gmail.com>
2021-10-08 12:14:50 -0500

    updated addition of the hover of the grid in tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,39 @@
 window.onload = function(){
     console.log('worked');
+
+    const statusDisplay = document.querySelector('#status');
+
+    //statusDisplay.innerHTML = 
+    
+    let gamestatus = true;
     let gameboard = document.getElementById('board');
     let grid = gameboard.getElementsByTagName('div');
-    
-    
+    let curplayer = "X";
+    let gameState = ["", "", "", "", "", "", "", "", ""];
+    let boxes = [];
+    const curplayerTurn = () => `It's ${curplayer}'s turn`;
+
+
+    statusDisplay.innerHTML = curplayerTurn();
+
+    //Adding the game baord and hover for each tile on the board.
     for (var r = 0; r < grid.length; r++) 
     {
         grid[r].classList.add('square');
         
+        grid[r].addEventListener('mouseover', function(e) {
+            e.target.classList.add('hover');
+            
+        });
+        grid[r].addEventListener('mouseout', function(e) {
+            e.target.classList.remove('hover');
+  
+        });
+        
     };
    
-  
+    
+    
+    
+    
 };
